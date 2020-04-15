d84581255eaf110dd0c70aef314f15bd347da7b0
toonnyy8 <toonnyy8@gmail.com>
2020-04-15 22:11:56 +0800

    update

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -5,64 +5,95 @@ import * as Tone from "tone"
 
 // import * as Piano from "../js/instruments/piano"
 
-let canvas = document.createElement("canvas")
-canvas.width = 500
-canvas.height = 500
+// let canvas = document.createElement("canvas")
+// canvas.width = 500
+// canvas.height = 500
 
-document.body.appendChild(canvas)
+// document.body.appendChild(canvas)
 
-let ctx = canvas.getContext("2d")
+// let ctx = canvas.getContext("2d")
 
+// {
+//     let gradient = ctx.createLinearGradient(0, 0, 0, 500);
+//     gradient.addColorStop(0, 'rgba(255,100,50,1)');
+//     gradient.addColorStop(0.5, 'rgba(50,255,255,1)');
+//     gradient.addColorStop(1, 'rgba(255,50,255,1)');
+//     ctx.fillStyle = gradient;
+//     ctx.rect(0, 0, 500, 500);
+//     ctx.fill();
+// }
+
+// {
+//     let gradient = ctx.createLinearGradient(0, 0, 500, 0);
+//     gradient.addColorStop(0, 'rgba(0,0,0,0.7)');
+//     gradient.addColorStop(1, 'rgba(0,0,0,0)');
+//     ctx.fillStyle = gradient;
+//     ctx.rect(0, 0, 500, 500);
+//     ctx.fill();
+// }
+
+// let down = false
+// let frequencies = new Array(200).fill(1).reduce((prev, curr) => {
+//     prev.push(curr + prev[prev.length - 1])
+//     return prev
+// }, [0])
+
+// var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
+// //set the attributes using the set interface
+// // synth.set("detune", -1200);
+// console.log(synth.voices)
+
+// canvas.onmousedown = (e => {
+//     console.log(e.offsetX)
+//     console.log(e.offsetY)
+//     synth.triggerAttack([(1 - e.offsetY / 500) * 1000])
+//     down = true
+// })
+
+// canvas.onmousemove = (e => {
+//     if (down) {
+//         console.log(e.offsetX)
+//         console.log(e.offsetY)
+//         synth.voices[0].frequency.value = (1 - e.offsetY / 500) * 1000
+//     }
+// })
+
+// canvas.onmouseup = (e => {
+//     down = false
+//     // synth.triggerRelease([0])
+//     synth.voices.map(voice => {
+//         voice.triggerRelease()
+//     })
+//     // synth.voices[0].frequency.value = 0
+// })
+
+let colors = ["yellow", "orange", "red", "pink", "purple", "blue", "teal", "green"]
+
+let container = document.createElement("div")// <HTMLDivElement>document.getElementById("container")
 {
-    let gradient = ctx.createLinearGradient(0, 0, 0, 500);
-    gradient.addColorStop(0, 'rgba(255,100,50,1)');
-    gradient.addColorStop(0.5, 'rgba(50,255,255,1)');
-    gradient.addColorStop(1, 'rgba(255,50,255,1)');
-    ctx.fillStyle = gradient;
-    ctx.rect(0, 0, 500, 500);
-    ctx.fill();
+    container.classList.add("w-160")
+    container.classList.add("h-120")
+    container.classList.add("grid")
+    container.classList.add("grid-rows-6")
+    container.classList.add("grid-flow-col")
+    container.classList.add("gap-0")
+    container.classList.add("col-gap-0")
 }
-
-{
-    let gradient = ctx.createLinearGradient(0, 0, 500, 0);
-    gradient.addColorStop(0, 'rgba(0,0,0,0.7)');
-    gradient.addColorStop(1, 'rgba(0,0,0,0)');
-    ctx.fillStyle = gradient;
-    ctx.rect(0, 0, 500, 500);
-    ctx.fill();
-}
-
-let down = false
-let frequencies = new Array(200).fill(1).reduce((prev, curr) => {
-    prev.push(curr + prev[prev.length - 1])
-    return prev
-}, [0])
-
-var synth = new Tone.PolySynth(6, Tone.Synth).toMaster();
-//set the attributes using the set interface
-// synth.set("detune", -1200);
-console.log(synth.voices)
-
-canvas.onmousedown = (e => {
-    console.log(e.offsetX)
-    console.log(e.offsetY)
-    synth.triggerAttack([(1 - e.offsetY / 500) * 1000])
-    down = true
-})
-
-canvas.onmousemove = (e => {
-    if (down) {
-        console.log(e.offsetX)
-        console.log(e.offsetY)
-        synth.voices[0].frequency.value = (1 - e.offsetY / 500) * 1000
+colors.forEach((color) => {
+    for (let i = 3; i < 9; i++) {
+        let button = document.createElement("button")
+        button.classList.add("w-20")
+        button.classList.add("h-20")
+        button.classList.add("focus:outline-none")
+        button.classList.add(`bg-${color}-${i}00`)
+        button.classList.add(`_nmp_`)
+        button.classList.add(`-nmp_src--lt`)
+        button.classList.add(`active:-nmp_light--${color}-${i - 1}00`)
+        button.classList.add(`active:-nmp_shadow--${color}-${i + 1}00`)
+        button.classList.add(`-nmp_dist--xs`)
+        button.classList.add(`active:-nmp_shape--inner`)
+        container.appendChild(button)
     }
 })
 
-canvas.onmouseup = (e => {
-    down = false
-    // synth.triggerRelease([0])
-    synth.voices.map(voice => {
-        voice.triggerRelease()
-    })
-    // synth.voices[0].frequency.value = 0
-})
+document.body.appendChild(container)
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,36 @@ module.exports = {
   important: false,
   separator: ':',
   theme: {
+    neomorphism: {
+      sources: {
+        "l": 0,
+        "lt": Math.PI * (1 / 4),
+        "t": Math.PI * (1 / 2),
+        "rt": Math.PI * (3 / 4),
+        "r": Math.PI,
+        "rb": Math.PI * (5 / 4),
+        "b": Math.PI * (3 / 2),
+        "lb": Math.PI * (7 / 4),
+      },
+      distances: {
+        "xs": "3px",
+        "sm": "7px",
+        "base": "11px",
+        "md": "15px",
+        "lg": "20px",
+        "xl": "25px",
+        "2xl": "30px",
+        "3xl": "35px",
+        "4xl": "40px",
+        "5xl": "45px",
+        "6xl": "50px"
+      },
+      blurs: {
+        "sm": (value) => `calc(1 * var(${value}))`,
+        "base": (value) => `calc(2 * var(${value}))`,
+        "md": (value) => `calc(3 * var(${value}))`
+      }
+    },
     screens: {
       sm: '640px',
       md: '768px',
@@ -603,6 +633,7 @@ module.exports = {
     },
   },
   variants: {
+    neomorphism: ['responsive', 'hover', 'focus', 'active'],
     accessibility: ['responsive', 'focus'],
     alignContent: ['responsive'],
     alignItems: ['responsive'],
@@ -693,5 +724,7 @@ module.exports = {
     transitionDuration: ['responsive'],
   },
   corePlugins: {},
-  plugins: [],
+  plugins: [
+    require('neomorphism-in-tailwind')
+  ],
 }
